889032901344ce47b3c32d0f8959ce473ff4831c
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-19 23:16:36 +0700

    [PHAN-02] Dinh nghia component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function One() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello guy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to see more
-        </a>
-      </header>
+  <div>
+    <h1>So 1 tap 1</h1>
+    <h5>So 1 tap cuoi</h5>
+  </div> );
+}
+
+var Two = function()
+{
+  return (
+    <div>
+    <h3> Cach so 2 </h3>
     </div>
   );
 }
+
+var Three = () =>(
+  <div>
+    <h5> Cach so 3 </h5>
+    </div>
+);
+
+class Four extends Component {
+  render() {
+    return (
+      <div>
+         <button> React JS component so 4 </button>
+      </div>
+    );
+  }
+}
+class Five extends Component {
+  render() {
+    return (
+      <div>
+         <button> React JS component so 5 </button>
+      </div>
+    );
+  }
+}
+
+class App  extends Component{
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <One></One>
+        <Two/>
+        <Three/>
+        <hr/>
+        <Four/>
+        <Five/>
+      </header>
+    </div>
+    );
+  }
+}
+
 
 export default App;
