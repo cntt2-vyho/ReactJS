8affe9080b36fbfa66edc27f309d7d84d7532eb2
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-19 23:31:14 +0700

    [PHAN-04] Thuc hanh JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const a1 = "nho";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello guy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to see more
-        </a>
-      </header>
-    </div>
-  );
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3>toi ten la : {a1} </h3>
+      </div>
+    )
+  }
+}
+
+//vd ve ham map
+const so = [1,2,3,4,5,6];
+const so2 = so.map((x) => x*2);
+class App2 extends Component {
+  render() {
+    return (
+      <div>
+         { so2 }
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App=header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <App2/>
+        </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload. 
+          </p>
+      </div>
+    );
+  }
 }
 
 export default App;
