344a39ba8a9fe212a591e2dcf1079327fa5a9085
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-20 00:04:27 +0700

    [PHAN-05] Thuc hanh component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopMenu from './Component/topmenu/TopMenu';
+import Header from './Component/header/Header';
+import Content from './Component/content/Content';
+import Footer from './Component/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello guy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to see more
-        </a>
-      </header>
+    <div className="_013">
+      <TopMenu/>
+      <Header/>
+      <Content title="For those about to rock..." position1="order-lg-2" position2="order-lg-1" anh="img/01.jpg" trichdan="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati."/>
+      <Content title="We salute you!" anh="img/02.jpg" trichdan="A simple, one page website template for quickly building one page websites using Bootstrap 4."/>
+      <Content title="Let there be rock!" position1="order-lg-2" anh="img/03.jpg" trichdan="Subscribe to get updated when new Bootstrap related products arrive!"/>
+      <Footer/>
     </div>
   );
 }
