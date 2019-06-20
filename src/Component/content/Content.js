a74d8982f4cfca938f301984b853690c72ac95da
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-20 15:29:49 +0700

    [PHAN-07] Khai niem va thao tac voi state

--- src/Component/content/Content.js
+++ src/Component/content/Content.js
@@ -1,30 +1,61 @@
 import React, { Component } from 'react';
 
 class Content extends Component {
-    constructor(props) {
-        super(props);
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      trangThai: 0
     }
 
+  }
 
-    thongbao =() => {
-      alert('Cach xu ly tuong tac trong React JS !')
+
+
+  editClick = () => {
+    this.setState({ trangThai: 1 });
+  }
+
+  saveClick = () => {
+    this.setState({ trangThai: 0 });
+  }
+
+  renderButton = () => {
+
+    <div className="row">
+      <div className="btn btn-group">
+        <div className="btn btn-info" onClick={() => this.editClick()} >Edit </div>
+        <div className="btn btn-warning">Remove</div>
+      </div>
+    </div>
+  };
+
+
+  renderForm = () => {
+    <div classname="row">
+      <div className="form-group">
+        <input defaultValue={this.props.tieude} type="text" name="ten" className="form-control" />
+      </div>
+      <div className="form-group">
+        <div className="btn" style={{ backgroundColor: 'black', color: 'white' }} onClick={() => this.saveClick}> Save </div>
+      </div>
+    </div>
+  };
+
+  displayCheck = () => {
+    if(this.state.trangThai===0)
+    {
+      return this.renderButton();
     }
+    else{ return this.renderForm();}
+  }
 
-    thongbao2 = () => {
-      alert('Thong bao so 2 !')
-    }
-
-    thongbao3 = (x) => {
-      alert(x)
-    };
-
-    render() {
-        return (
-            <section className="col4">
+  
+  render() {
+    return (
+      <section className="col4">
         <div className="container">
           <div className="row align-items-center">
-            <div className={"col-lg-6 " + this.props.position1}>
+            <div className={"col-lg-6 "}>
               <div className="p-5">
                 <img className="img-fluid rounded-circle" src={this.props.anh} alt />
               </div>
@@ -34,21 +65,15 @@ class Content extends Component {
                 <h2 className="display-4">{this.props.title}</h2>
                 <p>{this.props.trichdan}</p>
 
-                <div className="row">
-        <div className="btn btn-group">
-          <div className="btn btn-info" onClick={this.thongbao}>Edit</div>
-          <div className="btn btn-warning" onClick={() => this.thongbao2()}>Remove</div>
-          <div className="btn btn-info" onClick={() => this.thongbao3("ha ha")}>Here</div>
-          <div className="btn btn-warning" onClick={() => this.thongbao3.bind(this, "ahihi")}>There</div>
-        </div>
-      </div>
+                {this.displayCheck()}
+
               </div>
             </div>
           </div>
         </div>
       </section>
-        );
-    }
+    );
+  }
 }
 
 export default Content;
