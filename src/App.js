8ae9e2ba24260aa51726a3da476c61fd5e789393
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-25 16:56:18 +0700

    [PHAN-35] Phan tich cau truc mot ung dung redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    var redux = require('redux');
+    var oldSatate = {
+      num:["man hinh", "chuot", "ban phim"],
+      editStatus: true
+  }
+    var reducer1 = (state=oldSatate, action) => {
+      switch (action.type) {
+        case "CHANGE_EDIT_STATUS":
+          return {...state, editStatus:!state.editStatus};
+          break;
+      
+        default:
+          break;
+      }
+      return state;
+    }
+
+    
+    var store1 = redux.createStore(reducer1);
+    console.log(store1.getState());
+    store1.dispatch({type:"CHANGE_EDIT_STATUS"})
+    
+    console.log(store1.getState());
+
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +45,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
