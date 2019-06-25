ce758d36123377b202576a6b3972032cec0f9dfd
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-25 14:55:06 +0700

    [PHAN-34] Bat dau voi toan tu 3 cham

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+  render() {
+    // var p1 = {
+    //   ten : "man hinh",
+    //   gia: 90
+    // }
+    // console.log(p1);
+    // var p2={...p1};
+    // p2.gia=100;
+    // console.log(p2);
+
+    // var a1 = [1, 5, 3, 10];
+    // var a2 = [...a1];
+    // a2[2] = 7;
+    // console.log(a1);
+    // console.log(a2);
+
+
+    var b1 = {
+      num : [34,25,56],
+      status: true
+    };
+    var b2 = {...b1};
+    b2.status = false;
+
+    console.log(b2);
+
+    var b3 = {...b1, num :[...b1.num, 100]};
+    //b3.num[3] = 200;
+    console.log(b3);
+
+
+
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +53,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
