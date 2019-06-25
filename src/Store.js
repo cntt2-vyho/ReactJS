39f7c6402c236ccc3d034c387faf2ebf58b7efad
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-25 18:56:41 +0700

    [PHAN-38] 109 Cach chia tach Reducer thanh tung file

--- src/Store.js
+++ src/Store.js
@@ -1,33 +1,43 @@
+
+import numReducer from './reducers/numReducer';
+import editStatusReducer from './reducers/editStatusReducer';
+
 var redux = require('redux');
+
 var oldSatate = {
-  num:["man hinh", "chuot", "ban phim"],
-  editStatus: true
+    num: ["man hinh", "chuot", "ban phim"],
+    editStatus: true
 }
-var reducer1 = (state=oldSatate, action) => {
-  switch (action.type) {
-    case "CHANGE_EDIT_STATUS":
-      return {...state, editStatus:!state.editStatus};
+var reducer1 = (state = oldSatate, action) => {
+    switch (action.type) {
+        case "CHANGE_EDIT_STATUS":
+            return { ...state, editStatus: !state.editStatus };
 
-      case "ADD_NEW":
-      return {...state, num : [...state.num, action.newItem]};
+        case "ADD_NEW":
+            return { ...state, num: [...state.num, action.newItem] };
 
-      case "DELETE":
-      return {...state, num : state.num.filter( (value, i) => i !== action.index)};
-  
-    default:
-      return state;
-  }
-  
+        case "DELETE":
+            return { ...state, num: state.num.filter((value, i) => i !== action.index) };
+
+        default:
+            return state;
+    }
+
 }
 
 
-var store1 = redux.createStore(reducer1);
-store1.subscribe(()=> {
-  console.log(JSON.stringify(store1.getState()));
+const allReducer = redux.combineReducers({
+    num : numReducer,
+    editStatus : editStatusReducer
 })
 
-store1.dispatch({type:"CHANGE_EDIT_STATUS"});
-store1.dispatch({type:"ADD_NEW", newItem:"CPU"})
-store1.dispatch({type:"DELETE", index: 1});
+var store1 = redux.createStore(allReducer);
+store1.subscribe(() => {
+    console.log(JSON.stringify(store1.getState()));
+})
+
+store1.dispatch({ type: "CHANGE_EDIT_STATUS" });
+store1.dispatch({ type: "ADD_NEW", newItem: "CPU" })
+store1.dispatch({ type: "DELETE", index: 1 });
 
 export default store1;
