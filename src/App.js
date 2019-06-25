a9a3d019b27443c845ef0725281d0231f8341561
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-25 18:34:21 +0700

    [PHAN-38] 108 Truy xuat du lieu trong store tu component con

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import News from './News';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
@@ -9,12 +10,16 @@ class App extends Component {
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
       </header>
+      {this.props.dulieu}
       <News />
     </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    dulieu : state.num
+  }
+}
+export default connect(mapStateToProps)(App);
