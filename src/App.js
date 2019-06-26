a8f9ce96f24d3842ee30c8f9069e98756de393da
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-26 14:46:42 +0700

    [PHAN-41] Thuc hanh them sua xoa du lieu firebase tu reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import {firebaseConnect} from './firebaseConnect';
 
-function App() {
+class App extends Component {
+
+
+  pushData = () => {
+    var dataConnect = firebase.database().ref('dataForNote');
+    dataConnect.push({
+      title:"Ghi chu so 3",
+      content: "day la content cua note 3"
+    })
+    console.log('ban vua them moi du lieu');
+  }
+
+
+  removeData = (id) => {
+
+    var dataConnect = firebase.database().ref('dataForNote');
+    dataConnect.child(id).remove();
+    console.log('ban da xoa thanh cong phan tu cos if la: ' +id);
+  }
+
+  render() {
+  console.log(firebaseConnect);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           hello guy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here to see more
-        </a>
+        </h1>
       </header>
+      <button onClick={()=> this.pushData()} >Click to add database</button>
+      <hr />
+      <button onClick={()=> this.removeData('-LiHiPDA_7gs0vKyHLSi')} >Click to remove database</button>
     </div>
   );
+  }
 }
-
-export default App;
+export default App
---
+++ src/firebaseConnect.js
@@ -0,0 +1,29 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDRonkTeTFkwJBdRNUb-2SYkpNYmW2K2Rk",
+    authDomain: "notereactproj.firebaseapp.com",
+    databaseURL: "https://notereactproj.firebaseio.com",
+    projectId: "notereactproj",
+    storageBucket: "",
+    messagingSenderId: "417818283055",
+    appId: "1:417818283055:web:44ed0b231e344216"
+  };
+  
+
+  export const firebaseConnect = firebase.initializeApp(firebaseConfig);
+
+
+  var data = firebase.database().ref( 'dataForNote/note2');
+  // sửa dữ liệu
+  data.set({
+    id: 3,
+    title: "title cua note 2",
+    content: "hom nay tui ngu toi 10h sang"
+  })
+
+
+    //lấy dữ liệu
+    // data.once('value').then(function(snapshot){
+    //   console.log(snapshot.val());
+    // })
