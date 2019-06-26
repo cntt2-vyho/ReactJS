8fa701674d9f020ee963844fd01575bb27df8138
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-26 17:22:51 +0700

    [PHAN-41] Thuc hanh them sua xoa du lieu firebase tu reactjs

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
-import { noteData } from './firebaseConnect';
-import Nav from './Nav';
-import NoteList from './NoteList';
-import NoteForm from './NoteForm';
+import {firebaseConnect} from './firebaseConnect';
 
 class App extends Component {
 
 
-  constructor(props){
-    super(props);
-    this.state = {
-    }
+  pushData = () => {
+    var dataConnect = firebase.database().ref('dataForNote');
+    dataConnect.push({
+      title:"Ghi chu so 3",
+      content: "day la content cua note 3"
+    })
+    console.log('ban vua them moi du lieu');
   }
 
-  addData =  (item) => {
-    noteData.push(item);
+
+  removeData = (id) => {
+
+    var dataConnect = firebase.database().ref('dataForNote');
+    dataConnect.child(id).remove();
+    console.log('ban da xoa thanh cong phan tu cos if la: ' +id);
   }
+
   render() {
-
-
-    
-    return (
-      <div>
-        <Nav />
-        <div className="container">
-          <div className="row">
-            <NoteList />
-            <NoteForm getData ={ (item) => {this.addData(item)}} />
-          </div>
-        </div>
-      </div>
-    );
+  console.log(firebaseConnect);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          hello guy
+        </h1>
+      </header>
+      <button onClick={()=> this.pushData()} >Click to add database</button>
+      <hr />
+      <button onClick={()=> this.removeData('-LiHiPDA_7gs0vKyHLSi')} >Click to remove database</button>
+    </div>
+  );
   }
 }
 export default App
