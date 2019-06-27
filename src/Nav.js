833fc34a20d8d3ada2b29b436010ce33bc816dfc
myvyho39@gmail.com <myvyho39@gmail.com>
2019-06-27 21:21:09 +0700

    [PHAN-48] Xu ly dieu huong su dung store

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Nav extends Component {
+class Nav extends Component {
+
+
+  handleAdd = (event) => {
+    event.preventDefault();
+    this.props.changeEditStatus();
+  }
+
     render() {
         return (
             <nav className="navbar navbar-expand-sm navbar-dark" style={{backgroundColor: '#99004d', padding: '8px 250px'}}>
@@ -8,11 +16,12 @@ export default class Nav extends Component {
             <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
             <div className="collapse navbar-collapse" id="collapsibleNavId" style={{display: 'inline-block !important'}}>
               <ul className="navbar-nav mr-auto mt-2 mt-lg-0" style={{float: 'right'}}>
+
                 <li className="nav-item active">
                   <a className="nav-link" href="https://facebook.com">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item active">
-                  <a className="nav-link" href="https://facebook.com">Xem danh sách</a>
+                  <a className="nav-link" href="https://facebook.com" onClick={(event) => this.handleAdd(event)} >Add New</a>
                 </li>
               </ul>
             </div>
@@ -20,3 +29,16 @@ export default class Nav extends Component {
         )
     }
 }
+const mapStateToProps = (state, ownProps) => {
+  return {
+    
+  }
+}
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    changeEditStatus: () => {
+      dispatch({type:"CHANGE_EDIT_STATUS"})
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Nav) ;
